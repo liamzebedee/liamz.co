18caff17c2c6b87db9263fa80f6e45e2ef88f3aa
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2013-03-26 18:04:47 +1000

    update site

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
+	// Auto-affix
 	$('#thegrey-wrapper').height($("#thegrey-wrapper").height());
 	$('#thegrey').affix({
 		offset: $('#thegrey').position()
+	});
+	
+	// Responsive masthead
+	$('#masthead h1').fitText({
+		maximumFontSize: 80,
+	});
+	$('#masthead h2').fitText({
+		maximumFontSize: 24,
 	});
 });
