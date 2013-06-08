56791db4cebddcaf9b8d770595483705ec6e70f3
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2013-06-08 12:41:58 +1000

    update site

--- assets/global-ceffa6f05f04953e4cf9a1d25b914eb6.js
+++ assets/global-ceffa6f05f04953e4cf9a1d25b914eb6.js
@@ -1,8 +1,1 @@
-// Quote of the day
-$(document).ready(function() {
-	var qotd = $('#quote-of-the-day');
-	var allQuotes = $('#all-quotes li', qotd);
-	// Gets a random quote
-	var quote = allQuotes.eq(Math.floor(Math.random()*allQuotes.length)).text();
-	if(quote !== null) qotd.text(quote);
-});
+$(document).ready(function(){var qotd=$("#quote-of-the-day");var allQuotes=$("#all-quotes li",qotd);var quote=allQuotes.eq(Math.floor(Math.random()*allQuotes.length)).text();if(quote!==null){qotd.text(quote)}});
