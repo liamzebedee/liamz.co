0ab5838d997b847c7477c2d7be244eaf25d1135b
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2013-07-20 15:58:28 +1000

    update site

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,1 @@
-$(document).ready(function() {
-	// Quote of the day
-	var qotd = $('#quote-of-the-day');
-	var allQuotes = $('#all-quotes li blockquote', qotd);
-	// Gets a random quote
-	var quote = allQuotes.eq(Math.floor(Math.random()*allQuotes.length));
-	if(quote !== null) $('#quote', qotd).html(quote.html());
-	
-	
-	// Timeago
-	$("time.timeago").timeago();
-});
+$(document).ready(function(){var qotd=$("#quote-of-the-day");var allQuotes=$("#all-quotes li blockquote",qotd);var quote=allQuotes.eq(Math.floor(Math.random()*allQuotes.length));if(quote!==null){$("#quote",qotd).html(quote.html())}$("time.timeago").timeago()});
