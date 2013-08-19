70138250360fe44bd99d3b625687db10dd16b86d
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2013-08-19 19:09:22 +1000

    update _site

--- _plugins/ext.rb
+++ _plugins/ext.rb
@@ -1,17 +1,16 @@
 require 'jekyll_asset_pipeline'
-#require 'jekyll-press'
-
 module Jekyll
 	module Commands
 		# I overwrite this here so we only do heavy work (like compressing HTML and stuff)
 		# when we are building the site, not when testing (which uses jekyll serve)
 		class << Build
-			alias_method :_process, :process
-			def process(options)
-				require 'jekyll-press'
-				options['asset_pipeline']['bundle'] = true
-				options['asset_pipeline']['compress'] = true
-				_process(options)
+			alias_method :_build, :build
+			def build(site, options)
+				puts options['heavy_build']
+				if options['heavy_build']
+					require 'jekyll-press'
+				end
+				_build(site, options)
 			end
 		end
 	end
