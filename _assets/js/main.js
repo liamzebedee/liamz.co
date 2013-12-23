d99d394c6336bbb76d1d3354c95dfdfcb4bfdeb6
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2013-12-23 16:06:37 +1000

    update _site

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -2,15 +2,8 @@ $(document).ready(function() {
 	if (typeof window.console === 'object' && typeof window.console.log === 'function') {
     window.console.log('Salutations! My website is completely open-source, so you might find it easier to view the source at http://github.com/liamzebedee/liamz.co');
   }
-
-  // Quote of the day
-	var qotd = $('#quote-of-the-day');
-	var allQuotes = $('#all-quotes li blockquote', qotd);
-	// Gets a random quote
-	var quote = allQuotes.eq(Math.floor(Math.random()*allQuotes.length));
-	if(quote !== null) $('#quote', qotd).html(quote.html());
-
-
-	// Timeago
-	$("time.timeago").timeago();
+  $(".owl-carousel").owlCarousel({
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+      items : 1,
+  });
 });
